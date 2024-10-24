15bb9f725d8bba1d782da2bee6a604a40f936ff6
Ben Duncan <ben.duncan@komoot.de>
2024-10-24 14:23:45 +0200

    Create deploy.yml to deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import type { UserConfig } from 'vite';
 
 export default {
   base: '/',
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'public'),
   build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        nested: resolve(__dirname, 'src/test/index.html'),
       },
     },
   },
